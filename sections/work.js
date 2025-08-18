26431edfcba8f6accbb2f18032b88be458d5acc1
Mark O Meara <markomeara98@gmail.com>
2025-08-18 17:05:31 +0100

    added in work for drum game

--- sections/work.js
+++ sections/work.js
@@ -10,8 +10,52 @@ export const work = `
 
     <div style="margin-top: 20px">
 
-        <div style="margin-bottom: 20px">
-            
+        <div style="margin-bottom: 20px">  
+            <div style="display: flex; flex-direction: row; align-items: center; border-bottom: 1px dashed black; width: 100%">
+                <p style="margin: 0; font-size: 0.80rem; width: 5%; text-align: center;">1</p>
+                <h2 style="margin: 0; width: 65%; text-align: left; padding: 5px">Drum Rhythm Web Game</h3>
+                <p style="margin: 0; font-size: 0.80rem; width: 30%; text-align: center;";">Started: Aug 15, 2025</p>
+            </div>
+
+            <div style="width: fit-content; position: relative; margin: 10px;">
+                <img src="./images/drum_game_cover.png" alt="Portfolio Screenshot"
+                    style="display: block; max-width: 100%; max-height: 180px; object-fit: cover; border-radius: 3px; box-shadow: 0 2px 10px rgba(0,0,0,0.3);" />
+
+                <img src="./images/sela-tape.png"
+                    alt="Overlay"
+                    style="position: absolute; top: -20px; right: -30px; width: 80px; height: 60px; pointer-events: none;">
+
+                <img src="./images/sela-tape.png"
+                    alt="Overlay"
+                    style="position: absolute; top: -20px; left: -30px; width: 80px; height: 60px; pointer-events: none; transform: rotate(90deg);">
+            </div>
+
+            <p style="font-size: 1rem; margin-bottom: 10px;">
+            A web game built using Javascript, HTML and CSS where you have to hit the drums at right time to survive. See how high of a score you can get.
+            <br/>
+            <br/>
+            Features clean UI with custom made pixel art made using Aesprite. Also includes background music, sound effects and animations.
+            </p>
+
+            <p style="margin-bottom: 10px; font-size: 0.80rem; background-color: #c6f5f5; border-radius: 5px; padding: 8px; font-family: JetBrains-Mono, Arial, monospace;">
+               Javascript, HTML, CSS, Aesprite
+            </p>
+
+            <a href="https://github.com/markom9822/portfolio-website-vercel" target="_blank" style="display: flex; align-items: center;">
+                <svg stroke="blue" fill="none" stroke-width="1.5" viewBox="0 0 24 24" stroke-linecap="round"
+                stroke-linejoin="round" height="18px" width="18px" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4">
+                    </path>
+                    <path d="M9 18c-4.51 2-5-2-7-2">
+                    </path>
+                </svg>
+                <p style="margin: 0; margin-left: 2px; font-size: 1rem; text-align: center; color: blue; font-family: JetBrains-Mono, Arial, monospace">Github Link</p>
+            </a>
+
+        </div>
+
+        <div style="margin-bottom: 20px">  
             <div style="display: flex; flex-direction: row; align-items: center; border-bottom: 1px dashed black; width: 100%">
                 <p style="margin: 0; font-size: 0.80rem; width: 5%; text-align: center;">1</p>
                 <h2 style="margin: 0; width: 65%; text-align: left; padding: 5px">Full-stack portfolio website</h3>
