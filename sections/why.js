e302d131e0a763514e6373ccec8889d82d66a69b
Mark O Meara <markomeara98@gmail.com>
2025-08-22 10:26:57 +0100

    update to why section

--- sections/why.js
+++ sections/why.js
@@ -2,13 +2,14 @@ export const why = `
 <div style="flex: 1;">
     <h1>Why career change?</h1>
     <p>
-        I have a strong passion for building new things and continuously learning, this drives my desire to transition into a career as a web developer.
+        I have a strong passion for building new things and continuously learning.
         I love creating applications and find great satisfaction in building something myself, whether it's sharing my work with friends and family or
-        helping strangers. 
+        helping strangers. I have fallen in love with coding in the past few years getting great enjoyment in expressing my creativity like I did as a child
+        building with lego.
         <br/><br/>
         After gaining valuable experience in Unity VR development,
         I feel I have reached a ceiling in terms of growth and opportunities. I believe that moving into full-stack web development offers
-        better long-term career prospects and importantly a broad range of challenges that I am excited to get stuck into.
+        better long-term career prospects and most importantly a broad range of challenges that I am excited to get stuck into.
     </p>
 </div>
 `;
